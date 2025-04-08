98c050557d072a976311d7be04676786aecde315
konyogony <me@konyogony.dev>
2025-04-08 16:39:42 +0700

    very cool swtiching between virtual line and virtual text to better see the error

--- .config/nvim/lua/kony/keymaps.lua
+++ .config/nvim/lua/kony/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<C-w>", "<cmd>bd<CR>", { desc = "Close current buffer" })
 vim.keymap.set("n", "<C-t>", "<cmd>enew<CR>", { desc = "Open new empty buffer" })
 -- vim.keymap.set("n", "<C-s>", "<cmd>BufferLineCloseOther<CR>", { desc = "Close all other buffers, except for the current one" })
-vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeFocus<CR>", {desc = "Focus on NvimTree"})
+vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeFocus<CR>", { desc = "Focus on NvimTree" })
 vim.keymap.set("n", "<C-m>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 
 -- These two dont work for some reason, while the commands executed manually work perfectly fine. :/
@@ -12,3 +12,17 @@ for i = 1, 9 do
 	vim.keymap.set("n", "<C-" .. i .. ">", "<cmd>BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to buffer " .. i })
 end
 
+function toggle_diagnostics_virtual()
+	local current_config = vim.diagnostic.config()
+	if not current_config then
+		return
+	end
+	local new_config = not current_config.virtual_text
+
+	vim.diagnostic.config({
+		virtual_text = new_config,
+		virtual_lines = not new_config,
+	})
+end
+
+vim.keymap.set("n", "<C-d>", toggle_diagnostics_virtual, { desc = "Toggle diagnostics virtual text" })
