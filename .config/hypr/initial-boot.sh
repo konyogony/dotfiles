e2ae493d6d5bc280615c028225b96d7eca28652f
konyogony <me@konyogony.dev>
2025-04-17 00:30:27 +0700

    no need for hyprlock or hypridle

--- .config/hypr/initial-boot.sh
+++ .config/hypr/initial-boot.sh
@@ -11,21 +11,14 @@ mpvpaper_cmd="mpvpaper -o "no-audio --loop" '*' /home/kony/.config/hypr/wave.gif
 swww_cmd="swww img"
 effect="--transition-bezier .43,1.19,1,.4 --transition-fps 30 --transition-type grow --transition-pos 0.925,0.977"
 
-# Check if a marker file exists, not sure how or why this works
 if [ ! -f ~/.config/hypr/.initial_startup_done ]; then
-    # Initilize wallpaper using mpvpaper_cmd
-    # $mpvpaper_cmd > /dev/null 2>&1 &
-
     swww query || swww-daemon && $swww_cmd $wallpaper $effect
 
-    # initiate GTK dark mode and apply icon and cursor theme
     gsettings set org.gnome.desktop.interface color-scheme $color_scheme >/dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme >/dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface icon-theme $icon_theme >/dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface cursor-theme $cursor_theme >/dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface cursor-size 24 >/dev/null 2>&1 &
-
-    # initiate kvantum theme
     kvantummanager --set "$kvantum_theme" >/dev/null 2>&1 &
 
     # initiate the kb_layout (for some reason) waybar cant launch it, DO I NEED THIS??
